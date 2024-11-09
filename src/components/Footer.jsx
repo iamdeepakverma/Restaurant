dc09f6787d14ecba25d129a603718ed1d93cb486
Deepak <deepuverma124124@gmail.com>
2024-11-09 19:09:51 +0530

    2 commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,18 +5,17 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="banner">
-          <div className="left">ZEESHU</div>
+          <div className="left">Deeps</div>
           <div className="right">
-            <p>Z6 Gulshan-e-Maymar, Karachi</p>
-            <p>Open: 05:00 PM - 12:00 AM</p>
+            <p>Open: 09:00 AM - 11:00 PM</p>
           </div>
         </div>
         <div className="banner">
           <div className="left">
-            <p>Developed By CODEWITHZEESHU</p>
+            <p>Developed By Deepakverma</p>
           </div>
           <div className="right">
-            <p>All Rights Reserved By CodeWithZeeshu.</p>
+            <p>All Rights Reserved By DeepakVerma.</p>
           </div>
         </div>
       </div>
